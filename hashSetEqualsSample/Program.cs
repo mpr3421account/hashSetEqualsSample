e6b1e1af64cf43c89065dd4fd502e7cd40c32439
Marcos Ribeiro <marcos@azacessorios.com.br>
2023-02-03 11:16:40 -0300

    Demo 2 - SortedSet

--- hashSetEqualsSample/Program.cs
+++ hashSetEqualsSample/Program.cs
@@ -1,12 +1,37 @@
-﻿HashSet<string> set = new HashSet<string>();
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
-set.Add("TV");
-set.Add("Notebook");
-set.Add("Tablet");
+SortedSet<int> a = new SortedSet<int>() { 0, 2, 4, 5, 6, 8, 10 };
+SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
 
-Console.WriteLine(set.Contains("Notebook"));
+//union
+SortedSet<int> c = new SortedSet<int>(a);
+c.UnionWith(b);
+Console.WriteLine("Union");
+PrintCollection(c);
 
-foreach(String p in set)
+//intersection
+SortedSet<int> d = new SortedSet<int>(a);
+d.IntersectWith(b);
+Console.WriteLine("Intersection");
+PrintCollection(d);
+
+//difference
+SortedSet<int> e = new SortedSet<int>(a);
+e.ExceptWith(b);
+Console.WriteLine("Difference");
+PrintCollection(e);
+
+
+
+
+
+
+static void PrintCollection<T>(IEnumerable<T> collection)
 {
-    Console.WriteLine(p);
+    foreach(T obj in collection)
+    {
+        Console.Write(obj + " ");
+    }
+    Console.WriteLine();
 }
